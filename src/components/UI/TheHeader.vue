826a48cb49818a340540f3551fdc0fb7eefc96c7
Sudhanshu Singh <sudhanshu@datacollaborative.com>
2024-08-26 05:50:53 +0530

    pokemon cards made

---
+++ src/components/UI/TheHeader.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+  data() {
+    return {
+      navItems: [
+        "Home",
+        "Pokedox",
+        "Videgame",
+        "GCC Pokemon",
+        "TV Pokemon",
+        "Play Pokemon",
+        "News",
+      ],
+    };
+  },
+};
+</script>
+<template>
+  <header class="container">
+    <div class="nav-container">
+      <div class="nav-item" v-for="(item, idx) in navItems" :key="idx">
+        {{ item }}
+      </div>
+    </div>
+  </header>
+</template>
+<style scoped>
+.nav-container {
+  display: flex;
+  width: 100%;
+  justify-content: space-around;
+  background-color: #fff;
+  margin-top: 2rem;
+}
+.nav-item {
+    padding: 2rem;
+}
+</style>
+ 
---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<script>
+import PokemonList from "../components/PokemonList/PokemonList.vue";
+import TheHeader from "../components/UI/TheHeader.vue";
+export default {
+  components: { TheHeader, PokemonList },
+};
+</script>
+<template>
+  <div>
+    <TheHeader />
+    <PokemonList />
+  </div>
+</template>
+<style scoped></style>
